4076be37daee33c4782a19523ebe6e61bfa91451
RiRi <hyerin77@naver.com>
2022-10-25 16:04:57 +0900

    [요구사항1] index.html 응답하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,8 +28,23 @@ public class RequestHandler extends Thread {
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
+        	BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        	String line = br.readLine();
+        	log.debug("reqeust line : {}",line);
+        	if(line==null) {
+        		return;
+        	}
+        	
+        	String[] tokens = line.split(" ");
+        	
+        	while(!line.equals("")) {
+        		line = br.readLine();
+        		log.debug("header : {}", line);
+        	}
+        	
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World".getBytes();
+//            byte[] body = "Hello World".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp"+tokens[1]).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
